946be938a005c831205c29d6ac21aecb39d0e4a0
antonin <antonin.lesaout@icloud.com>
2023-06-05 23:51:23 +0200

    Fix formers request mapping

--- src/main/java/com/tpjava/tpjava2/controller/FormerController.java
+++ src/main/java/com/tpjava/tpjava2/controller/FormerController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
-@RequestMapping("/former")
+@RequestMapping("/formers")
 public class FormerController {
 
     @Autowired
